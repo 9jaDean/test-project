dfe1a3ea5004810abcf1722a537a1cb20355160f
9jaDean <onuohasamuelpro@gmail.com>
2024-09-05 14:55:06 +0100

    Changed the bg color to aqua and modified the js file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function updateMessage() {
     const messageElement = document.getElementById('message');
-    messageElement.textContent = 'The message has been updated by JavaScript!';
+    messageElement.textContent = 'The message has been updated by You!';
 }
 
 document.getElementById('updateMessageButton').addEventListener('click', updateMessage);
